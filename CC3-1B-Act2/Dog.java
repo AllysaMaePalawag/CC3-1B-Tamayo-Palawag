55083ca288bcaf62a9764348fe85f9a054100722
AllysaMaePalawag <44884958+AllysaMaePalawag@users.noreply.github.com>
2019-01-16 14:48:26 +0800

    Add files via upload

---
+++ CC3-1B-Act2/Dog.java
@@ -0,0 +1,52 @@
+package classesandobjects;
+
+public class Dog {
+    
+    String name;
+    int age;
+    char gender;
+    String breed;
+
+
+    public void setName(String dogName){
+        
+        name= dogName;
+        
+    }
+    
+    public String getName(){
+     
+        return name;
+    }
+     public void setAge(int dogAge){
+        
+        age= dogAge;
+        
+    }
+    
+    public int getAge(){
+        
+        return age;
+    }
+    
+    public void setGender(char dogGender){
+       
+        gender= dogGender;
+        
+    }
+    public char getGender(){
+        
+        return gender;
+    }
+    
+     public void setBreed(String dogBreed){
+        
+        breed= dogBreed;
+        
+    }
+    
+    public String getBreed(){
+     
+        return breed;
+    }
+}
